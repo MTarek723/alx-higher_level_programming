072ea80424322551bd2dac5d1b7b640713f7a43d
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-11-08 11:13:17 -0800

    L

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#?usr/bin/python3
 def best_score(a_dictionary):
     sum1 = 0
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     else:
         for x in a_dictionary:
             if a_dictionary[x] > sum1:
                 sum1 = a_dictionary[x]
                 sum2 = x
-        return sum2
+    return sum2
