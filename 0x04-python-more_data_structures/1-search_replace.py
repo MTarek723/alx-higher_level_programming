5d85fa1645af5a1c5be443215a0b1dcca21b5cf5
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-11-08 10:03:54 -0800

    L

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list[:]
-    x = new.index(search)
-    new.remove(search)
-    new.insert(x, replace)
+    new = []
+    for x in my_list:
+        if x == search:
+            new.append(replace)
+        else:
+            new.append(x)
     return new
