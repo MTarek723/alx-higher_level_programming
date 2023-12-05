622fed69ebd244f1e7c78b8f6b45076bf24b1393
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-12-05 10:48:44 -0800

    L

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""defination of class Student."""
 
 
 class Student:
-    """Represent a student."""
+    """represen student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
+        """initialization of new Student.
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
+    def to_json(self, attrs=None):
+        """get dictionary representation of Student.
+        If attributess is list of strs, it represents only the attributes
+        included in list.
+        Args:
+            attrs (list): (Optional) attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+
+"""read from standard input then compute metrics.
+every ten lines or input of keyboard interruption (CTRL + C),
+print that statistics:
+    - total file size up to this point.
+    - Count of read status codes up to this point.
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
+    """print accumulative metrics.
     Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+        size (int): accumulated read file size.
+        status_codes (dict): accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
 
 if __name__ == "__main__":
     import sys
