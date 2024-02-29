5f5e298a162e398a5a35d6ca75a6268a00007f77
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-02-29 09:30:54 -0800

    L

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#a Bash script that sends a DELETE request to the URL passed 
-curl -sx DELETE "$1"
+# Send a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-def find_peak(list_of_integers):
-    left = 0
-    right = len(list_of_integers) - 1
+"""
+   this function finds a peak in a list of unsorted integers
+"""
 
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
 
-    return list_of_integers[left]
+def find_peak(numbr):
+    '''
+        Finds the peak in a list of numbers
+    '''
+    length = len(numbr)
+    if length == 0:
+        return None
+    if length == 1:
+        return (numbr[0])
+    if length == 2:
+        return numbr[0] if numbr[0] >= numbr[1] else numbr[1]
+
+    for idx in range(0, length):
+        value = numbr[idx]
+        if (idx > 0 and idx < length - 1 and
+                numbr[idx + 1] <= value and numbr[idx - 1] <= value):
+                return value
+        elif idx == 0 and numbr[idx + 1] <= value:
+            return value
+        elif idx == length - 1 and numbr[idx - 1] <= value:
+            return value
+    return pick
