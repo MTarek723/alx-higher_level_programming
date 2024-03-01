ab537b44335c4a4347f34476b612eedb8b3c5173
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-01 10:58:57 -0800

    L

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request to the passed URL
+takes in a URL and an email address, sends a POST request to the passed URL
 parameter, and finally displays the body of the response
 """
 import requests
