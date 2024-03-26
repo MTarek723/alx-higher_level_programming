676c4189dd8ecdffb764e5d8076f4724e91f9c65
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-26 13:47:32 -0700

    L

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function(error, content) {
-    console.log(error || content);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
