97401061817aa0d84224bb3cf9e7dad5a8a2e51f
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-26 13:54:54 -0700

    L

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], "utf-8", function(error) {
     if (error) {
-        console.log(error)
+        console.log(error);
     }
-})
+});
+
+
